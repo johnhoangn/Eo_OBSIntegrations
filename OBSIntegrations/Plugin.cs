cb59a14ad76ae096eaf5286513e8e82baeb01b3d
John Nguyen <johnhoangn@gmail.com>
2021-11-04 23:07:44 -0400

    initial

---
+++ OBSIntegrations/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace OBSIntegrations
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Plugin.Log.Info("OnApplicationStart");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+
+    }
+}
